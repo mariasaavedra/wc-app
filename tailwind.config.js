741600979d7b710d678977bf59094bb5d770349e
Shanice <shanicegipson13@gmail.com>
2022-03-12 13:53:07 -0600

    added upadated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-gold': '#BE9619',
+        'brand-Magenta': '#7F2362',
+        'brand-blue': '#304C78',
+        'brand-green': '#117E58',
+        'brand-purple': '#5955A4',
+      }, 
+      fontFamily: {
+        RedHat: ['Red Hat Display', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
