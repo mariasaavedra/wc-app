d3a969c663fb4907a8e3c0d6c7477e5d55257612
Shanice <shanicegipson13@gmail.com>
2022-03-21 18:43:42 -0500

    fixed navigation, centered button, resized card on mobile

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -4,29 +4,7 @@ import { Text } from "../components/Text";
 
 const EventsPage: NextPage = () => {
   return (
-    <div className="mx-auto">
-       <div className="bg-brand-gold">
-        <div className="flex flex-col justify-center py-8 px-8 text-justify lg:px-14">
-        <h1 className="flex justify-center text-2xl pb-3.5 lg:text-5xl lg:pb-12 lg:pt-5">
-          ABOUT US
-        </h1>
-          <p className="lg:pb-20 lg:px-40 lg:text-lg">
-          Our mission is to give youth the opportunity to learn technology concepts, and leadership skills; creating a pipeline of future-ready professionals through project-based learning and innovative programs.
-WE CODE KC is an initiative that serves the urban core of Kansas City, exposing youth to various programming languages, technology, cyber security, and computer science concepts. Learning the basic concept of coding develops creativity, problem solving skills, builds confidence and helps to build a positive future. Every child, no matter their zip code or their family’s financial status, should have the opportunity to learn to code.
-          </p>
-        </div>
-    </div>
-    <div className="cards-container columns-1 md:columns-2 sm:columns-2 pt-20 flex justify-around flex-col lg:flex-row">
-        <div className="card-container w-96 flex flex-col">
-          <img className="card-image mx-auto pb-5" src="https://source.unsplash.com/random/600x600/?hackathon"></img>
-          <button className="card-button w-8/12 bg-brand-blue text-white">OUR TEAM</button>
-        </div>
-        <div className="card-container w-96 flex flex-col">
-          <img className="card-image mx-auto pb-5" src="https://source.unsplash.com/random/600x600/?tech"></img>
-          <button className="card-button w-8/12 bg-brand-blue text-white">OUR BOARD</button>
-        </div>
-      </div>
-    </div>
+    <></>
   );
 }
 
