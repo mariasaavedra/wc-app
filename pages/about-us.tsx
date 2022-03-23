14e0ea7f7d12aa97bb8c7fa893035e0685afb47e
Shanice <shanicegipson13@gmail.com>
2022-03-22 21:11:22 -0500

    added footer component

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,7 +1,10 @@
+import { Footer } from "./components/Footer";
+
 const AboutPage = () => {
   return (
     <div className="container mx-auto">
       <p>About Page</p>
+      <Footer />
     </div>
   );
 };
