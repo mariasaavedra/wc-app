cb951de91d0d06277a6c7c0cc24835eb91c25ada
Shanice <shanicegipson13@gmail.com>
2022-03-25 21:15:30 -0500

    added route and fixed js code in board

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -19,7 +19,7 @@ WE CODE KC is an initiative that serves the urban core of Kansas City, exposing 
         <div className="card-container  pb-12  w-80 lg:w-96 flex flex-col">
           <img className="card-image mx-auto pb-5" src="https://source.unsplash.com/random/600x600/?hackathon"></img>
           <Link href={'/team'}>
-          <a className="card-button w-8/12 bg-brand-blue text-white mx-auto p-2.5 uppercase text-center">OUR TEAM</a>
+            <a className="card-button w-8/12 bg-brand-blue text-white mx-auto p-2.5 uppercase text-center">OUR TEAM</a>
           </Link>
         </div>
         <div className="card-container w-80 lg:w-96 flex flex-col">
