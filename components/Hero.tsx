74d9f7d7bc9974e438506909f5c2e318f829701e
Maria Saavedra <msaav3@gmail.com>
2022-03-28 20:42:26 -0500

    Align center hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import { MaybeParentComponentProps } from "../interfaces/ParentComponentProps";
 import styles from "../styles/components/Hero.module.scss";
+import Text from './Text';
 
 interface HeroProps extends MaybeParentComponentProps {
 
@@ -7,10 +8,10 @@ interface HeroProps extends MaybeParentComponentProps {
 
 export const Hero = (props: HeroProps) => {
   return (
-    <div>
-      <p className={styles.hero}>
-          {props.children}
-      </p>
+    <div className={styles.hero}>
+      <div className={styles.innerWrapper}>
+          <Text size="h1">{props.children}</Text>
+      </div>
     </div>
   );
 };
