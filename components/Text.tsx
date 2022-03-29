58060ffba976710e64f7af83e42f29387e4b7d75
Maria Saavedra <msaav3@gmail.com>
2022-03-28 20:30:58 -0500

    Updated text component

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,23 +1,92 @@
-import styles from "../styles/Text.module.scss";
-interface TextProps {
-  children: any;
+import { ParentComponentProps } from '../interfaces/ParentComponentProps';
+import styles from '../styles/components/Text.module.scss';
+
+type TextSize = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'b1' | 'b2' | 'b3';
+
+export interface TextProps extends ParentComponentProps {
+    /**
+     * Heading (h) or Body (b), with relative size
+     */
+    size?: TextSize;
 }
 
-export const getClasses = (): string => {
-  const classes = [styles.ResponsiveBlockComponent];
-  return classes.join(" ");
-};
-
-export const Text = (props: TextProps) => {
-  return (
-    <div className={styles.text + " text-xl text-center p-10 grid grid-flow-col"}>
-      <div>
-        {props.children}
-      </div>
-    </div>
-  );
-};
-
-Text.defaultProps = {
-  grid: 1,
-};
+/**
+ * Standard element for displaying typography
+ */
+export default function Text(props: TextProps) {
+    const getClasses = () => {
+        const classes = props.className?.split(' ') || [];
+        const sizesWithStyles: Array<TextSize> = ['b1', 'b2', 'b3'];
+        if (props.size && sizesWithStyles.includes(props.size)) {
+            classes.push(styles[props.size]);
+        }
+        return classes.join(' ');
+    };
+    const renderText = (content: any) => {
+        const classes = getClasses();
+        switch (props.size) {
+            case 'b1':
+                return (
+                    <p style={props.style} className={classes}>
+                        {content}
+                    </p>
+                );
+            case 'b2':
+                return (
+                    <p style={props.style} className={classes}>
+                        {content}
+                    </p>
+                );
+            case 'b3':
+                return (
+                    <p style={props.style} className={classes}>
+                        {content}
+                    </p>
+                );
+            case 'h1':
+                return (
+                    <h1 style={props.style} className={classes}>
+                        {content}
+                    </h1>
+                );
+            case 'h2':
+                return (
+                    <h2 style={props.style} className={classes}>
+                        {content}
+                    </h2>
+                );
+            case 'h3':
+                return (
+                    <h3 style={props.style} className={classes}>
+                        {content}
+                    </h3>
+                );
+            case 'h4':
+                return (
+                    <h4 style={props.style} className={classes}>
+                        {content}
+                    </h4>
+                );
+            case 'h5':
+                return (
+                    <h5 style={props.style} className={classes}>
+                        {content}
+                    </h5>
+                );
+            case 'h6':
+                return (
+                    <h6 style={props.style} className={classes}>
+                        {content}
+                    </h6>
+                );
+            default:
+                return (
+                    <p style={props.style} className={classes}>
+                        {content}
+                    </p>
+                );
+        }
+    };
+    const display = renderText(props.children);
+    return <div className={styles.TextComponent}>{display}</div>;
+}
