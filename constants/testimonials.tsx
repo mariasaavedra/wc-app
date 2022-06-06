e15ec97fb63dd0e90a0dade44cff1d64b06ba6bb
Tim DeGraffenreid <90429693+timdegraffenreid@users.noreply.github.com>
2022-06-06 09:21:29 -0500

    add brandon zubia image

--- constants/testimonials.tsx
+++ constants/testimonials.tsx
@@ -4,13 +4,13 @@ const TESTIMONIALS = [
       title: "",
       testimonial:
         "I am very grateful to have received the opportunity to work with KC Scholars and WeCode KC. I have learned about different technologies while being in the program and would recommend it to anyone who is interested in learning more about tech.",
-      image: undefined,
+      image: "/images/brandon-zubia.jpg",
       },
     {
-      name: "Parent",
+      name: "Dacia Martin",
       title: "",
       testimonial:
-        "Connor is proud of himself. When he was younger and Connor would sing math songs or the planet songs kids at the playground and kids thought that he was weird. WeCode gives him opportunity to be himself in a place where being smart is cool &#x263A",
+        "Connor is proud of himself. When he was younger and Connor would sing math songs or the planet songs kids at the playground and kids thought that he was weird. WeCode gives him opportunity to be himself in a place where being smart is cool.",
       image: undefined,
     },
   ];
