6825c5d72843edbb3251c0af21de8587dcc9020b
Tim DeGraffenreid <90429693+timdegraffenreid@users.noreply.github.com>
2022-06-21 10:19:39 -0500

    add hero to board page

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,25 +1,35 @@
+import Hero from '../components/Hero';
+import Text from '../components/Text';
+import {Button} from '../components/Button';
+
 const BoardPage = () => {
   return (
     <div className="mx-auto">
-      <div className="header bg-brand-purple">
-        <div className="flex flex-col justify-center py-8 px-8 text-justify lg:px-14">
-          <h1 className="flex justify-center text-2xl pb-3.5 lg:text-5xl lg:pb-12 lg:pt-5">
-            OUR BOARD
-          </h1>
-          <p className="lg:pb-20 lg:px-40 lg:text-lg">
-            Our mission is to give youth the opportunity to learn technology
-            concepts, and leadership skills; creating a pipeline of future-ready
-            professionals through project-based learning and innovative
-            programs. WE CODE KC is an initiative that serves the urban core of
-            Kansas City, exposing youth to various programming languages,
-            technology, cyber security, and computer science concepts. Learning
-            the basic concept of coding develops creativity, problem solving
-            skills, builds confidence and helps to build a positive future.
-            Every child, no matter their zip code or their family’s financial
-            status, should have the opportunity to learn to code.
-          </p>
-        </div>
-      </div>
+      <Hero title="Our Board">
+        <>
+          <Text size="b1" className="mt-2 mb-8">
+          Our mission is to give youth the opportunity to learn technology concepts,
+          and leadership skills; creating a pipeline of future-ready professionals through
+          project-based learning and innovative programs. WE CODE KC is an initiative that
+          serves the urban core of Kansas City, exposing youth to various programming languages,
+          technology, cyber security, and computer science concepts.
+          </Text>
+          
+          <Text size="b1" className='mt-2'>
+          Learning the basic concept of coding develops creativity, 
+          problem solving skills, builds confidence and helps to build a positive future.
+          Every child, no matter their zip code or their family’s financial status, should have the opportunity to learn to code.
+          </Text>
+          <br />
+          <Button
+            className="mt-8"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdYfOQ_31AsFUE9HKPkI45diPVndUdWah8eH8eep90HiON9qA/viewform"
+          >
+            Join Our Team
+          </Button>
+        </>
+      </Hero>
+ 
       <div className="content-container">
         <section className="flex px-24 pt-24 items-center">
           <img
