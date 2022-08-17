fc1c2056ea2e0fee6a9158bbdaf381d63b5632ca
Maria Saavedra <msaav3@gmail.com>
2022-08-17 00:02:54 -0500

    remove summer events container

--- pages/events.tsx
+++ pages/events.tsx
@@ -26,69 +26,6 @@ const Events: NextPage = () => {
           </Text>
         </>
       </Hero>
-
-      <div className="mx-auto container my-4 pt-5  px-5 text-left">
-        <div className="md:w-1/2 mx-auto text-center my-8">
-          <h1 className="text-5xl text-center bold">Summer of Code</h1>
-
-          <p className="my-4">
-            Mon, Aug 1, 2022, 9:00 AM – Fri, Aug 5, 2022, 4:00 PM CDT
-          </p>
-
-          <p className="my-4">5008 Prospect Avenue Kansas City, MO 64130</p>
-
-          <Text className="mt-4" size="b1">
-            These classes are hands-on workshops that focus on teaching the
-            beginner, intermediate, and advanced students the technology
-            concepts shown above. Classes starts DAILY at 9am and end at 4pm.
-            Lunch will be provided for each student.
-          </Text>
-          <Text size="b2" className="mt-4">
-            <strong>Sign for Summer of Code classes below:</strong>
-          </Text>
-        </div>
-      </div>
-      <div className=" ml-8 mt-0 md:mx-auto container overflow-x-scroll grid grid-rows-1 grid-flow-col">
-        <EventCard
-          title="Build-a-Laptop"
-          href="https://www.eventbrite.com/e/summer-of-code-with-wecode-kc-tickets-354095869277"
-        >
-          Students will learn how to build-a-laptop using special hardware and
-          then will program it. Laptops will NOT be given to students upon
-          completion of the class. Scholarships are available under special
-          circumstances
-        </EventCard>
-        <EventCard
-          title="Roblox"
-          href="https://www.eventbrite.com/e/summer-of-code-with-wecode-kc-tickets-354095869277"
-        >
-          Students will create their own games in their own virtual studios.
-          Scholarships are available under special circumstances for students.
-        </EventCard>
-        <EventCard
-          title="Virtual Reality"
-          href="https://www.eventbrite.com/e/summer-of-code-with-wecode-kc-tickets-354095869277"
-        >
-          Students will explore VR by programming a virtual environment using VR
-          headsets. Students will NOT be given headsets upon completion.
-          Scholarships are available to qualifying students.
-        </EventCard>
-        <EventCard
-          title="Raspberry Pi Jam"
-          href="https://www.eventbrite.com/e/summer-of-code-with-wecode-kc-tickets-354095869277"
-        >
-          Students will program Raspberry Pi hardware using the Python
-          programming language. Students will NOT be given Raspberry Pi hardware
-          upon completion. Scholarships are available to qualifying students.
-        </EventCard>
-        <EventCard
-          title="Graphic Design"
-          href="https://www.eventbrite.com/e/summer-of-code-with-wecode-kc-tickets-354095869277"
-        >
-          Students will develop professional graphics such as logos, banners,
-          and more! Scholarships are available to qualifying students.
-        </EventCard>
-      </div>
     </div>
   );
 };
