17c97d68574d5a7708501aa44f544bcf16c34ca6
Tim DeGraffenreid <90429693+timdegraffenreid@users.noreply.github.com>
2023-04-28 09:50:51 -0500

    add partners page shell

---
+++ pages/partners.tsx
@@ -0,0 +1,21 @@
+import { Hero } from "../components/Hero";
+import Text from "../components/Text";
+
+const PartnersPage = () => {
+  return (
+    <div className="mx-auto">
+      <Hero title="Partners">
+        <>
+          <Text size="b1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec tincidunt praesent semper feugiat nibh sed pulvinar. Id neque aliquam vestibulum morbi blandit. Nunc non blandit massa enim nec dui.          </Text>
+          <br />
+          <Text size="b1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec tincidunt praesent semper feugiat nibh sed pulvinar. Id neque aliquam vestibulum morbi blandit. Nunc non blandit massa enim nec dui.
+          </Text>
+        </>
+      </Hero>
+    </div>
+  );
+};
+
+export default PartnersPage;
